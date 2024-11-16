1eef19271987696b21ba9d768cee8e140f9a5dd7
Zoilo Granda <zgranda@gmail.com>
2024-11-16 09:28:19 -0400

    getphotos object params

--- src/api/external/getPhotos.ts
+++ src/api/external/getPhotos.ts
@@ -1,23 +1,29 @@
 import { Photo } from "@/api/types/Photo";
 
-export async function getPhotos(
-  limit: number = 25,
-  start: number = 0,
-  title?: string,
-): Promise<Photo[]> {
-  try {
-    let url = `https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_start=${start}`;
-    if (title) {
-      url += `&title=${encodeURIComponent(title)}`;
-    }
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Photos response was not ok");
-    }
-    const data: Photo[] = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Failed to fetch photos:", error);
-    throw error;
+interface GetPhotosParams {
+ limit?: number;
+ start?: number;
+ title?: string;
+}
+
+export async function getPhotos({
+ limit = 25,
+ start = 0,
+ title,
+}: GetPhotosParams = {}): Promise<Photo[]> {
+ try {
+  let url = `https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_start=${start}`;
+  if (title) {
+   url += `&title_like=${encodeURIComponent(title)}`;
   }
+  const response = await fetch(url);
+  if (!response.ok) {
+   throw new Error("Photos response was not ok");
+  }
+  const data: Photo[] = await response.json();
+  return data;
+ } catch (error) {
+  console.error("Failed to fetch photos:", error);
+  throw error;
+ }
 }
