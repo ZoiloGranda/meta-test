9768aadac00a1cf9434d0d9b0fae48bde75413a0
Zoilo Granda <zgranda@gmail.com>
2024-11-16 12:28:58 -0400

    handle empty string

--- src/app/layouts/MainWrapper.tsx
+++ src/app/layouts/MainWrapper.tsx
@@ -15,9 +15,9 @@ interface MainWrapperProps {
 const MainWrapper: React.FC<MainWrapperProps> = ({ photos }) => {
   const [filteredPhotos, setFilteredPhotos] = useState(photos);
   const [isLoading, setIsLoading] = useState(false);
-  const [photoTitleFilter, setPhotoTitleFilter] = useState(false);
-  const [albumTitleFilter, setAlbumTitleFilter] = useState(false);
-  const [userEmailFilter, setUserEmailFilter] = useState(false);
+  const [photoTitleFilter, setPhotoTitleFilter] = useState("");
+  const [albumTitleFilter, setAlbumTitleFilter] = useState("");
+  const [userEmailFilter, setUserEmailFilter] = useState("");
 
   const handleUserEmailChange = async ({
     filters,
@@ -32,7 +32,7 @@ const MainWrapper: React.FC<MainWrapperProps> = ({ photos }) => {
     console.log("userAlbums", userAlbums);
     const userPhotos = await getPhotos({ albumId: userAlbums[0].id });
     console.log("userPhotos", userPhotos);
-    setUserEmailFilter(true);
+    setUserEmailFilter(value);
     setIsLoading(false);
     setFilteredPhotos(userPhotos);
   };
@@ -44,24 +44,28 @@ const MainWrapper: React.FC<MainWrapperProps> = ({ photos }) => {
     filters: FiltersObject;
     value: string;
   }) => {
+    if (!value) {
+      setAlbumTitleFilter("");
+      if (photoTitleFilter) {
+        handlePhotoTitleChange({ value: photoTitleFilter });
+      }
+      return;
+    }
     const filteredAlbums = await getAlbums({ [filters.field]: value });
     console.log("filteredAlbums", filteredAlbums);
-
     const albumPhotosPromises = filteredAlbums.map((album) =>
       getPhotos({ albumId: album.id }),
     );
     const allAlbumPhotos = await Promise.all(albumPhotosPromises);
     let aggregatedPhotos = allAlbumPhotos.flat();
     console.log("aggregatedPhotos", aggregatedPhotos);
-
     if (photoTitleFilter) {
       const albumIds = filteredAlbums.map((album) => album.id);
       aggregatedPhotos = filteredPhotos.filter((photo) =>
         albumIds.includes(photo.albumId),
       );
     }
-
-    setAlbumTitleFilter(true);
+    setAlbumTitleFilter(value);
     setIsLoading(false);
     setFilteredPhotos(aggregatedPhotos);
   };
@@ -69,7 +73,7 @@ const MainWrapper: React.FC<MainWrapperProps> = ({ photos }) => {
   const handlePhotoTitleChange = async ({ value }: { value: string }) => {
     const photosByTitle = await getPhotos({ title: value });
     console.log("photosByTitle", photosByTitle);
-    setPhotoTitleFilter(true);
+    setPhotoTitleFilter(value);
     setIsLoading(false);
     setFilteredPhotos(photosByTitle);
   };
