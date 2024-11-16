45254141e9dd8403ad40feb4c2722260000d32f3
Zoilo Granda <zgranda@gmail.com>
2024-11-16 09:24:47 -0400

    debounce input

--- src/app/layouts/MainFilters.tsx
+++ src/app/layouts/MainFilters.tsx
@@ -1,22 +1,36 @@
 "use client";
 import InputFilter from "@/app/components/InputFilter";
-import React from "react";
+import React, { useCallback, useRef } from "react";
 
 interface MainFiltersProps {
   onFilterChange: (filter: string, value: string) => void;
 }
 
 const MainFilters: React.FC<MainFiltersProps> = ({ onFilterChange }) => {
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const debounce = useCallback(
+    (filter: string, value: string) => {
+      if (debounceTimeout.current) {
+        clearTimeout(debounceTimeout.current);
+      }
+      debounceTimeout.current = setTimeout(() => {
+        onFilterChange(filter, value);
+      }, 300);
+    },
+    [onFilterChange],
+  );
+
   return (
     <>
       <div className="flex flex-col">
-        <InputFilter label="Photo Title" onFilterChange={onFilterChange} />
+        <InputFilter label="Photo Title" onFilterChange={debounce} />
       </div>
       <div className="flex flex-col">
-        <InputFilter label="Album Title" onFilterChange={onFilterChange} />
+        <InputFilter label="Album Title" onFilterChange={debounce} />
       </div>
       <div className="flex flex-col">
-        <InputFilter label="User Email" onFilterChange={onFilterChange} />
+        <InputFilter label="User Email" onFilterChange={debounce} />
       </div>
     </>
   );
