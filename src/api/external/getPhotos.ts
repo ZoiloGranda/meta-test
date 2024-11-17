ea7c047ebcf06546c206d75124a98ba64eb79afc
Zoilo Granda <zgranda@gmail.com>
2024-11-17 12:18:22 -0400

    update getphotos URLSearchParams

--- src/api/external/getPhotos.ts
+++ src/api/external/getPhotos.ts
@@ -1,41 +1,43 @@
 import { Photo } from "@/api/types/Photo";
 
 interface GetPhotosParams {
- limit?: number;
- start?: number;
- title?: string;
- albumId?: number[];
- page?: number;
+  limit?: number;
+  start?: number;
+  title?: string;
+  albumId?: number[];
+  page?: number;
 }
 
 export async function getPhotos({
- limit = 25,
- start = 0,
- title,
- albumId,
- page,
+  limit = 25,
+  start = 0,
+  title,
+  albumId,
+  page,
 }: GetPhotosParams = {}): Promise<Photo[]> {
- try {
-  let url = `https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_start=${start}`;
-  if (title) {
-   url += `&title_like=${encodeURIComponent(title)}`;
+  try {
+    const params = new URLSearchParams({
+      _limit: limit.toString(),
+      _start: start.toString(),
+      ...(title ? { title_like: title } : {}),
+      ...(page ? { _page: String(page) } : {}),
+    });
+    if (albumId && albumId.length > 0) {
+      albumId.forEach((id) => params.append("albumId", id.toString()));
+    }
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/photos?${params.toString()}`,
+    );
+    //
+
+    if (!response.ok) {
+      throw new Error("Photos response was not ok");
+    }
+    const data: Photo[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch photos:", error);
+    throw error;
   }
-  if (albumId && albumId.length > 0) {
-   albumId.forEach(id => {
-    url += `&albumId=${id}`;
-   });
-  }
-  if (page) {
-   url += `&_page=${page}`;
-  }
-  const response = await fetch(url);
-  if (!response.ok) {
-   throw new Error("Photos response was not ok");
-  }
-  const data: Photo[] = await response.json();
-  return data;
- } catch (error) {
-  console.error("Failed to fetch photos:", error);
-  throw error;
- }
 }
