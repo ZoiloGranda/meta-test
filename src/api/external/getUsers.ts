44f5f5830e2c207ed247799bcaedb78e17c015ee
Zoilo Granda <zgranda@gmail.com>
2024-11-17 12:20:19 -0400

    update URLSearchParams

--- src/api/external/getUsers.ts
+++ src/api/external/getUsers.ts
@@ -1,28 +1,31 @@
 import { User } from "@/api/types/User";
 
 interface GetusersParams {
- limit?: number;
- start?: number;
- email?: string;
+  limit?: number;
+  start?: number;
+  email?: string;
 }
 
 export async function getUsers({
- limit = 25,
- start = 0,
- email,
+  limit = 25,
+  start = 0,
+  email,
 }: GetusersParams = {}): Promise<User[]> {
- try {
-  let url = `https://jsonplaceholder.typicode.com/users?_limit=${limit}&_start=${start}`;
-  if (email) {
-   url += `&email=${encodeURIComponent(email)}`;
+  try {
+    const params = new URLSearchParams({
+      _limit: limit.toString(),
+      _start: start.toString(),
+      ...(email ? { email: email } : {}),
+    });
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?${params.toString()}`,
+    );
+    if (!response.ok) {
+      throw new Error("Users response was not ok");
+    }
+    const users: User[] = await response.json();
+    return users;
+  } catch (error) {
+    throw new Error(`Failed to fetch users: ${error}`);
   }
-  const response = await fetch(url);
-  if (!response.ok) {
-   throw new Error("Users response was not ok");
-  }
-  const users: User[] = await response.json();
-  return users;
- } catch (error) {
-  throw new Error(`Failed to fetch users: ${error}`);
- }
 }
