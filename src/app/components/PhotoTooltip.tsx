6c0ef5b71d47555af1e101f853a6b72319cdd55c
Zoilo Granda <zgranda@gmail.com>
2024-11-18 00:24:09 -0400

    titles

--- src/app/components/PhotoTooltip.tsx
+++ src/app/components/PhotoTooltip.tsx
@@ -8,9 +8,11 @@ interface PhotoTooltipProps {
 const PhotoTooltip: React.FC<PhotoTooltipProps> = ({ photoWithMetadata }) => {
   return (
     <div className="absolute left-1/2 z-50 mt-2 -translate-x-1/2 transform whitespace-nowrap rounded bg-black bg-opacity-75 p-2 text-white">
-      <h4 className="text-lg font-semibold">{photoWithMetadata?.title}</h4>
-      <p className="text-sm">{photoWithMetadata?.album.title}</p>
-      <p className="text-sm">{photoWithMetadata?.album.user.email}</p>
+      <h4 className="text-lg font-semibold">
+        Title: {photoWithMetadata?.title}
+      </h4>
+      <p className="text-sm">Album: {photoWithMetadata?.album.title}</p>
+      <p className="text-sm">Email: {photoWithMetadata?.album.user.email}</p>
     </div>
   );
 };
